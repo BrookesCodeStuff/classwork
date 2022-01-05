a336af1a06e78f1961443040882060de9cd24ee7
Brooke <brookepaglia@gmail.com>
2022-01-04 21:04:46 -0500

    add classwork for Module 2 and Module 3.1

---
+++ Class 4/02-Iteration/script.js
@@ -0,0 +1,11 @@
+// TODO: Write Your JavaScript Code Here
+// Store student names in variable
+students = ['Charlotte', 'Leah', 'Chris', 'Santiago', 'Boris'];
+
+// Log number of studdents to console
+console.log(students.length);
+
+// For each student's name, log greeting to the console using a for loop
+for (let i = 0; i < students.length; i++) {
+  console.log('Great to see you, ' + students[i] + '!');
+}
